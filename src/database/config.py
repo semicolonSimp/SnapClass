d0d6e6441f769e613e22fea16d0874538031b965
jainroshan785-blip <jainroshan785@gmail.com>
2026-08-11 16:14:02 +0530

    update config.py

--- src/database/config.py
+++ src/database/config.py
@@ -1,9 +1,7 @@
-import streamlit as st
-
-
+import os
 from supabase import create_client, Client
 
 supabase: Client = create_client(
-    st.secrets["SUPABASE_URL"], 
-    st.secrets["SUPABASE_KEY"]
+    os.getenv("SUPABASE_URL"), 
+    os.getenv("SUPABASE_KEY")
 )
