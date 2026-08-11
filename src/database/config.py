bbc5b09554c1fc849374762a8abef37b8ec769a9
jainroshan785-blip <jainroshan785@gmail.com>
2026-08-11 16:23:52 +0530

    update cnfig

--- src/database/config.py
+++ src/database/config.py
@@ -1,7 +1,16 @@
 import os
 from supabase import create_client, Client
 
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+if not SUPABASE_URL:
+    raise ValueError("SUPABASE_URL environment variable is missing")
+
+if not SUPABASE_KEY:
+    raise ValueError("SUPABASE_KEY environment variable is missing")
+
 supabase: Client = create_client(
-    os.getenv("SUPABASE_URL"), 
-    os.getenv("SUPABASE_KEY")
+    SUPABASE_URL,
+    SUPABASE_KEY
 )
